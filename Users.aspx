d70a825b632821c390d931d2cd842fdf6ea8747e
k8aqi <ignatiuseng08@gmail.com>
2026-01-12 17:46:36 +0800

    User Management
    suspend,view history(redirects to UserHistory.aspx),remove user

---
+++ Users.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Users.aspx.cs" Inherits="Respace.Users" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>User Management</h2>
+    <asp:GridView ID="gvUsers" runat="server" AutoGenerateColumns="False" 
+        OnRowCommand="gvUsers_RowCommand" CssClass="table table-striped">
+        <Columns>
+            <asp:BoundField DataField="UserID" HeaderText="ID" />
+            <asp:BoundField DataField="FullName" HeaderText="Name" />
+            <asp:BoundField DataField="Email" HeaderText="Email" />
+            <asp:BoundField DataField="UserType" HeaderText="Role (Owner/Renter)" />
+            <asp:BoundField DataField="Status" HeaderText="Status" />
+        
+           <asp:TemplateField HeaderText="Actions">
+            <ItemTemplate>
+                <asp:Button ID="btnApprove" runat="server" Text="Approve Owner" 
+                    CommandName="ApproveUser" CommandArgument='<%# Eval("UserID") %>' 
+                    Visible='<%# Eval("UserType").ToString() == "Owner" && Convert.ToBoolean(Eval("IsApproved")) == false %>' 
+                    CssClass="btn btn-success btn-sm" />
+
+                <asp:Button ID="btnSuspend" runat="server" Text="Suspend" 
+                    CommandName="SuspendUser" CommandArgument='<%# Eval("UserID") %>' 
+                    Visible='<%# Eval("Status").ToString() == "Active" %>' 
+                    CssClass="btn btn-warning btn-sm" />
+
+                <asp:Button ID="btnActivate" runat="server" Text="Activate" 
+                    CommandName="ActivateUser" CommandArgument='<%# Eval("UserID") %>' 
+                    Visible='<%# Eval("Status").ToString() == "Suspended" %>' 
+                    CssClass="btn btn-primary btn-sm" />
+
+                <asp:Button ID="btnHistory" runat="server" Text="View History" 
+                    CommandName="ViewHistory" CommandArgument='<%# Eval("UserID") %>' 
+                    CssClass="btn btn-info btn-sm" />
+
+                <asp:Button ID="btnDelete" runat="server" Text="Remove" 
+                    CommandName="DeleteUser" CommandArgument='<%# Eval("UserID") %>' 
+                    CssClass="btn btn-danger btn-sm" 
+                    OnClientClick="return confirm('Are you sure you want to remove this user?');" />
+
+
+            </ItemTemplate>
+        </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ Users.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Respace
+{
+    public partial class Users : System.Web.UI.Page
+    {
+        string connStr = ConfigurationManager.ConnectionStrings[DbConfig.ConnectionStringName].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadUsers();
+            }
+        }
+
+        private void LoadUsers()
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                // VERSATILE QUERY: Translates THEIR database names into YOUR GridView names
+                string query = $@"SELECT 
+                    {DbConfig.UserId} AS UserID, 
+                    {DbConfig.UserName} AS FullName, 
+                    {DbConfig.UserEmail} AS Email, 
+                    {DbConfig.UserType} AS UserType, 
+                    {DbConfig.UserStatus} AS Status, 
+                    {DbConfig.UserIsApproved} AS IsApproved 
+                    FROM {DbConfig.UserTable}";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvUsers.DataSource = dt;
+                gvUsers.DataBind();
+            }
+        }
+
+        protected void gvUsers_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ViewHistory")
+            {
+                Response.Redirect("UserHistory.aspx?UserId=" + e.CommandArgument);
+                return;
+            }
+
+            int userId = Convert.ToInt32(e.CommandArgument);
+            string sql = "";
+
+            // Use DbConfig constants to build the logic
+            switch (e.CommandName)
+            {
+                case "ApproveUser":
+                    sql = $"UPDATE {DbConfig.UserTable} SET {DbConfig.UserIsApproved} = 1, {DbConfig.UserStatus} = 'Active' WHERE {DbConfig.UserId} = @id";
+                    break;
+                case "SuspendUser":
+                    sql = $"UPDATE {DbConfig.UserTable} SET {DbConfig.UserStatus} = 'Suspended' WHERE {DbConfig.UserId} = @id";
+                    break;
+                case "ActivateUser":
+                    sql = $"UPDATE {DbConfig.UserTable} SET {DbConfig.UserStatus} = 'Active' WHERE {DbConfig.UserId} = @id";
+                    break;
+                case "DeleteUser":
+                    sql = $"DELETE FROM {DbConfig.UserTable} WHERE {DbConfig.UserId} = @id";
+                    break;
+            }
+
+            if (!string.IsNullOrEmpty(sql))
+            {
+                using (SqlConnection con = new SqlConnection(connStr))
+                {
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.Parameters.AddWithValue("@id", userId);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                LoadUsers();
+            }
+        }
+    }
+}
