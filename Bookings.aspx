adbfb2bdccc6f50d95d0a51c590e9bcfc32d901a
k8aqi <ignatiuseng08@gmail.com>
2026-01-12 17:21:48 +0800

    Booking Management
    accept/remove pending bookings

---
+++ Bookings.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Bookings.aspx.cs" Inherits="Respace.Bookings" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <h2>Booking Management</h2>
+        <p>Monitor and manage all space reservations across the platform.</p>
+
+        <asp:GridView ID="gvBookings" runat="server" CssClass="table table-striped table-bordered" 
+            AutoGenerateColumns="False" DataKeyNames="BookingID" OnRowDeleting="gvBookings_RowDeleting">
+            <Columns>
+                <asp:BoundField DataField="BookingID" HeaderText="ID" />
+                <asp:BoundField DataField="ListingID" HeaderText="Space ID" />
+                <asp:BoundField DataField="RenterID" HeaderText="User ID" />
+                <asp:BoundField DataField="BookingDate" HeaderText="Date" DataFormatString="{0:yyyy-MM-dd}" />
+                <asp:BoundField DataField="Status" HeaderText="Status" />
+                
+                <asp:TemplateField HeaderText="Actions">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="btnDelete" runat="server" CommandName="Delete" 
+                            OnClientClick="return confirm('Are you sure you want to cancel/remove this booking?');" 
+                            CssClass="btn btn-outline-danger btn-sm">Remove</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ Bookings.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Respace
+{
+    public partial class Bookings : System.Web.UI.Page
+    {
+        string connStr = ConfigurationManager.ConnectionStrings[DbConfig.ConnectionStringName].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadBookings();
+            }
+        }
+
+        private void LoadBookings()
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                // SQL ALIASING: We select THEIR column name but call it OUR name (BookingID, etc.)
+                // This prevents "Column not found" errors in the GridView later.
+                string sql = $@"SELECT 
+                                {DbConfig.BookingId} AS BookingID, 
+                                {DbConfig.BookingListingId} AS ListingID, 
+                                {DbConfig.BookingRenterId} AS RenterID, 
+                                {DbConfig.BookingDate} AS BookingDate, 
+                                {DbConfig.BookingStatus} AS Status 
+                                FROM {DbConfig.BookingTable} 
+                                ORDER BY {DbConfig.BookingDate} DESC";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+                con.Open();
+                gvBookings.DataSource = cmd.ExecuteReader();
+                gvBookings.DataBind();
+            }
+        }
+
+        protected void gvBookings_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // GridView uses the aliased name 'BookingID' as the DataKey
+            int bookingId = Convert.ToInt32(gvBookings.DataKeys[e.RowIndex].Value);
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                // Versatile DELETE using DbConfig table and ID names
+                string sql = $"DELETE FROM {DbConfig.BookingTable} WHERE {DbConfig.BookingId} = @id";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@id", bookingId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadBookings();
+        }
+    }
+}
