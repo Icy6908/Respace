fa6bba3917d80e2497cbb25f8429955d5b63495b
k8aqi <ignatiuseng08@gmail.com>
2026-01-12 17:24:24 +0800

    My Main Page
    hosts very basic kpi atm (will add more stuff next time)

---
+++ Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Respace.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <h2 class="mb-4">Admin Dashboard Overview</h2>
+        
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card text-white bg-primary mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Users</h5>
+                        <p class="card-text fs-2"><asp:Label ID="lblTotalUsers" runat="server" Text="0"></asp:Label></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card text-white bg-warning mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Pending Spaces</h5>
+                        <p class="card-text fs-2"><asp:Label ID="lblPendingSpaces" runat="server" Text="0"></asp:Label></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Revenue</h5>
+                        <p class="card-text fs-2"><asp:Label ID="lblRevenue" runat="server" Text="$0.00"></asp:Label></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card text-white bg-danger mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Open Queries</h5>
+                        <p class="card-text fs-2"><asp:Label ID="lblOpenQueries" runat="server" Text="0"></asp:Label></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Respace.App_Code;
+
+namespace Respace
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string connStr = ConfigurationManager.ConnectionStrings[DbConfig.ConnectionStringName].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadDashboardStats();
+            }
+        }
+
+        private void LoadDashboardStats()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connStr))
+                {
+                    con.Open();
+
+                    //Count Total Users 
+                    string sqlUsers = $"SELECT COUNT(*) FROM {DbConfig.UserTable}";
+                    SqlCommand cmdUsers = new SqlCommand(sqlUsers, con);
+                    lblTotalUsers.Text = cmdUsers.ExecuteScalar().ToString();
+
+                    //Count Pending Listings 
+                    // use the status column name from DbConfig
+                    string sqlPending = $"SELECT COUNT(*) FROM {DbConfig.ListingTable} WHERE {DbConfig.ListingStatus} = 'Pending'";
+                    SqlCommand cmdPending = new SqlCommand(sqlPending, con);
+                    lblPendingSpaces.Text = cmdPending.ExecuteScalar().ToString();
+
+                    //Sum Total Payment
+                    string sqlRev = $"SELECT SUM({DbConfig.PaymentAmount}) FROM {DbConfig.PaymentTable} WHERE {DbConfig.PaymentStatus} = 'Completed'";
+                    SqlCommand cmdRev = new SqlCommand(sqlRev, con);
+                    object rev = cmdRev.ExecuteScalar();
+                    lblRevenue.Text = (rev != DBNull.Value) ? string.Format("{0:C}", rev) : "$0.00";
+
+                    // 4. Count Open Support Queries (Versatile)
+                    string sqlSupport = $"SELECT COUNT(*) FROM {DbConfig.SupportTable} WHERE {DbConfig.SupportStatus} = 'Open'";
+                    SqlCommand cmdSupport = new SqlCommand(sqlSupport, con);
+                    lblOpenQueries.Text = cmdSupport.ExecuteScalar().ToString();
+                }
+            }
+            catch (SqlException ex)
+            {
+                //Log it instead of just writing to screen
+                lblTotalUsers.Text = "Error";
+                lblPendingSpaces.Text = "Error";
+                //keep the message for debugging integration
+                System.Diagnostics.Debug.WriteLine("Database Error: " + ex.Message);
+            }
+        }
+    }
+}
