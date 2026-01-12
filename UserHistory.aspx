06073df53115ae8b09db026fc5b5706b3b044145
k8aqi <ignatiuseng08@gmail.com>
2026-01-12 17:42:32 +0800

    User History
    see owned listings if owner and see booking history if renter

---
+++ UserHistory.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UserHistory.aspx.cs" Inherits="Respace.UserHistory" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Activity History for User ID: <asp:Label ID="lblUserId" runat="server" /></h2>
+    <hr />
+
+    <h3>Owned Listings (Spaces)</h3>
+    <asp:GridView ID="gvUserListings" runat="server" CssClass="table table-bordered" AutoGenerateColumns="True">
+        <EmptyDataTemplate>No listings owned by this user.</EmptyDataTemplate>
+    </asp:GridView>
+
+    <br />
+
+    <h3>Booking History (As Renter)</h3>
+    <asp:GridView ID="gvUserBookings" runat="server" CssClass="table table-bordered" AutoGenerateColumns="False">
+        <Columns>
+            <asp:BoundField DataField="BookingID" HeaderText="Ref Number" />
+            <asp:BoundField DataField="ListingID" HeaderText="Space ID" />
+            <asp:BoundField DataField="BookingDate" HeaderText="Date Reserved" DataFormatString="{0:d}" />
+            <asp:BoundField DataField="TotalPrice" HeaderText="Amount Paid" DataFormatString="{0:C}" />
+            <asp:BoundField DataField="Status" HeaderText="Current Status" />
+        </Columns>
+        <EmptyDataTemplate>No bookings found for this user.</EmptyDataTemplate>
+    </asp:GridView>
+    
+    <br />
+    <asp:Button ID="btnBack" runat="server" Text="Back to Users" CssClass="btn btn-secondary" OnClick="btnBack_Click" />
+</asp:Content>
---
+++ UserHistory.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Respace
+{
+    public partial class UserHistory : System.Web.UI.Page
+    {
+        string connStr = ConfigurationManager.ConnectionStrings[DbConfig.ConnectionStringName].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string userId = Request.QueryString["UserId"];
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    lblUserId.Text = userId;
+                    LoadHistory(userId);
+                }
+            }
+        }
+
+        private void LoadHistory(string userId)
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                // 1. Load Listings (Owned by this user)
+                // Use Aliasing so even if DB has 'fld_title', UI sees 'Title'
+                string listQuery = $@"SELECT 
+                    {DbConfig.ListingId} AS ListingID, 
+                    {DbConfig.ListingTitle} AS Title, 
+                    {DbConfig.ListingCategory} AS Category, 
+                    {DbConfig.ListingPrice} AS PricePerDay, 
+                    {DbConfig.ListingStatus} AS Status 
+                    FROM {DbConfig.ListingTable} 
+                    WHERE {DbConfig.ListingOwnerId} = @id";
+
+                SqlDataAdapter daList = new SqlDataAdapter(listQuery, con);
+                daList.SelectCommand.Parameters.AddWithValue("@id", userId);
+                DataTable dtList = new DataTable();
+                daList.Fill(dtList);
+                gvUserListings.DataSource = dtList;
+                gvUserListings.DataBind();
+
+                // 2. Load Bookings (Made by this user as a Renter)
+                try
+                {
+                    string bookQuery = $@"SELECT 
+                        {DbConfig.BookingId} AS BookingID, 
+                        {DbConfig.BookingListingId} AS ListingID, 
+                        {DbConfig.BookingDate} AS BookingDate, 
+                        {DbConfig.BookingTotal} AS TotalPrice, 
+                        {DbConfig.BookingStatus} AS Status 
+                        FROM {DbConfig.BookingTable} 
+                        WHERE {DbConfig.BookingRenterId} = @id";
+
+                    SqlDataAdapter daBook = new SqlDataAdapter(bookQuery, con);
+                    daBook.SelectCommand.Parameters.AddWithValue("@id", userId);
+                    DataTable dtBook = new DataTable();
+                    daBook.Fill(dtBook);
+                    gvUserBookings.DataSource = dtBook;
+                    gvUserBookings.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    // Log error to debug console instead of crashing
+                    System.Diagnostics.Debug.WriteLine("Booking Load Error: " + ex.Message);
+                }
+            }
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Users.aspx");
+        }
+    }
+}
